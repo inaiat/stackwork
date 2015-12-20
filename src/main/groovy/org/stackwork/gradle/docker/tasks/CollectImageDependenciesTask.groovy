2f96db5de159c4da1d1a2d1138a519dae993c026
Quinten Krijger <qkrijger@gmail.com>
2015-12-20 12:25:26 +0100

    temporary code

--- src/main/groovy/org/stackwork/gradle/docker/tasks/CollectImageDependenciesTask.groovy
+++ src/main/groovy/org/stackwork/gradle/docker/tasks/CollectImageDependenciesTask.groovy
@@ -10,7 +10,15 @@ class CollectImageDependenciesTask extends Copy {
     description = 'Copies the Docker dependencies to the build folder to make them available during an image build'
     group = 'Stackwork'
 
-    from project.configurations.stackwork
-    into "${project.buildDir}/stackwork-deps"
+    // doLast {
+      project.configurations.stackwork.resolvedConfiguration.resolvedArtifacts.each {
+        from it.file
+        into project.file("${project.buildDir}/stackwork-deps")
+        def filename = it.name + (it.classifier ? '-' + it.classifier : '') + '.' + it.extension
+        println "Preparing build/docker-artifacts/${filename}"
+        rename it.file.name, filename
+      }
+    // }
   }
+
 }
